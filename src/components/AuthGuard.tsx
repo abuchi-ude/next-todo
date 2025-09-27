57a0d08c1beecbb6ac3085289400e593537da7b2
Ude Maduabuchi <abuchi.ude11@gmail.com>
2025-09-27 18:25:54 +0100

    fixed bugs with nested route

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -25,7 +25,7 @@ const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
   }, [router]);
 
   if (loading) {
-    return <div className="overlay"><div className="spinner animate-spin rounded-full h-6 w-6 border-2 border-primary border-t-transparent"></div><p className='mt-[10px] text-lg text-primary'>Checking authentication...</p></div>;
+    return <div className="overlay"><div className="spinner animate-spin rounded-full h-6 w-6 border-2 border-primary border-t-transparent"></div><p className='mt-[10px] text-lg text-primary'>Loading...</p></div>;
   }
 
   return <>{children}</>;
